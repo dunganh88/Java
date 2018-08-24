2db7c59cad9384897ac3a78aec15f4fd0606bd5f
dunganh88 <dunganhbbr8897@gmail.com>
2018-08-24 22:16:03 +0700

    test

--- src/Java/Test.java
+++ src/Java/Test.java
@@ -2,7 +2,7 @@ package Java;
 
 public class Test {
 	public static void main(String[] args) {
-		Person person = new Person("Anh Dung");
+		Person person = new Person("Dung Anh");
 		System.out.println("Ten : " + person.getName() + "\ntuoi : " + person.getAge() + "\nDia chi : " + person.getAddress());
 	}
 }
